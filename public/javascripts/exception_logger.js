da8a651cd62796085c74cae33666b242b7191db5
Chris Wise <cwise@murmurinformatics.com>
2011-03-21 14:39:51 -0400

    fixed an issue with CSRF token on posts

--- public/javascripts/exception_logger.js
+++ public/javascripts/exception_logger.js
@@ -1,4 +1,16 @@
 $(function () {
+	function CSRFProtection(xhr) {
+	    var token = $('meta[name="csrf-token"]').attr('content');
+	    if (token) xhr.setRequestHeader('X-CSRF-Token', token);
+	};
+
+	function ajax_headers() {
+		if ('ajaxPrefilter' in $) $.ajaxPrefilter(function(options, originalOptions, xhr){ CSRFProtection(xhr) });
+		else $(document).ajaxSend(function(e, xhr){ CSRFProtection(xhr) });
+	};
+	
+	ajax_headers();	
+	
 	$('.show_link').live('click', function(event) {
 		$.get($(this).attr("href"));
 		return false;
