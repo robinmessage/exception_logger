1fb5d34eea2d2088b87ad68d527c3dc38e42c80a
Alex Bevilacqua <alexbevi@gmail.com>
2012-06-06 14:26:41 -0400

    removed a few historic files and began conversion for use as a mountable engine

--- lib/exception_logger/engine.rb
+++ lib/exception_logger/engine.rb
@@ -1,18 +1,24 @@
-require "exception_logger"
-require "rails"
-require "will_paginate"
-require "i18n"
-require "squeel"
-
 module ExceptionLogger
-  class Engine < Rails::Engine
-    ## Better: TODO create rake task to copy files to application/public
-    initializer "static assets" do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
-    end
-    rake_tasks do
-      load File.expand_path(File.dirname(__FILE__) + '/../railties/tasks.rake')
-    end
-
+  class Engine < ::Rails::Engine
+    isolate_namespace ExceptionLoggerEngine
   end
 end
+
+# require "exception_logger"
+# require "rails"
+# require "will_paginate"
+# require "i18n"
+# require "squeel"
+
+# module ExceptionLogger
+#   class Engine < Rails::Engine
+#     ## Better: TODO create rake task to copy files to application/public
+#     initializer "static assets" do |app|
+#       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+#     end
+#     rake_tasks do
+#       load File.expand_path(File.dirname(__FILE__) + '/../railties/tasks.rake')
+#     end
+
+#   end
+# end
