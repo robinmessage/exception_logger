f2570d5ee78e933acab2ccc1cb00f39b89944b20
Alex Bevilacqua <alexbevi@gmail.com>
2012-06-07 09:02:20 -0400

    fix search functionality

--- app/controllers/exception_logger/application_controller.rb
+++ app/controllers/exception_logger/application_controller.rb
@@ -1,5 +1,9 @@
 module ExceptionLogger
   class ApplicationController < ActionController::Base
     layout "exception_logger/layouts/logged_exceptions"
+
+      include ExceptionLogger::ExceptionLoggable 
+      rescue_from Exception, :with => :log_exception_handler # tells rails to forward the 'Exception' (you can change the type) to the handler of the module  
+
   end
 end
