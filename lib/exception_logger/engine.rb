7b29cd4de3b4ff6b580fae74c370fa878d87e78f
Alex Bevilacqua <alexbevi@gmail.com>
2012-06-07 09:39:06 -0400

    add assets to precompile list
    based on https://github.com/alexfarrill/exception_logger/commit/f1e7598309fdc184a2fdabdf9bae61c6325efc37

--- lib/exception_logger/engine.rb
+++ lib/exception_logger/engine.rb
@@ -1,24 +1,11 @@
 module ExceptionLogger
   class Engine < ::Rails::Engine
     isolate_namespace ExceptionLogger
+
+    if Rails.version > "3.1"
+      initializer "ExceptionLogger precompile hook" do |app|
+        app.config.assets.precompile += ['exception_logger.js', 'exception_logger.css']
+      end
+    end
   end
 end
-
-# require "exception_logger"
-# require "rails"
-# require "will_paginate"
-# require "i18n"
-# require "squeel"
-
-# module ExceptionLogger
-#   class Engine < Rails::Engine
-#     ## Better: TODO create rake task to copy files to application/public
-#     initializer "static assets" do |app|
-#       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
-#     end
-#     rake_tasks do
-#       load File.expand_path(File.dirname(__FILE__) + '/../railties/tasks.rake')
-#     end
-
-#   end
-# end
