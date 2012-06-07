97d83df6719e6e7e250e7cae4c263d33e41e82a3
Alex Bevilacqua <alexbevi@gmail.com>
2012-06-07 07:59:17 -0400

    more routing fixes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ ExceptionLogger::Engine.routes.draw do
   match "/query" => "logged_exceptions#query", :as => :query, :via => :post
   match "/destroy_all" => "logged_exceptions#destroy_all", :as => :destroy_all, :via => :post
   match "/feed" => "logged_exceptions#feed", :as => :feed, :via => :get
+  match "/:id" => "logged_exceptions#show", :via => :get
+  match "/:id" => "logged_exceptions#destroy", :via => :delete
 end
