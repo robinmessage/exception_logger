8076fea2d4d4cb829d1af4cc1c5bb3b7292772b1
Robin Message <robin@handyelephant.com>
2013-06-05 17:47:10 +0100

    Add index to user_id

--- db/migrate/20120607190915_add_user_to_logged_exceptions.rb
+++ db/migrate/20120607190915_add_user_to_logged_exceptions.rb
@@ -1,5 +1,6 @@
 class AddUserToLoggedExceptions < ActiveRecord::Migration
   def change
     add_column :logged_exceptions, :user_id, :integer, :default => nil
+    add_index :logged_exceptions, :user_id
   end
 end
